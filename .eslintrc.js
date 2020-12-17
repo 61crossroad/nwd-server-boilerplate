65550823c82e3684e75538278c63781237bc2b26
smallbee3 <smallbee3@gmail.com>
2020-12-17 17:16:33 +0900

    Refactor .eslintrc.js with plugins and rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,41 @@
 module.exports = {
   env: {
+    node: true,
     browser: true,
-    es2021: true
   },
   extends: [
-    'standard'
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended', // eslint-plugin-prettier + eslint-config-prettier
+    'prettier/@typescript-eslint', // Deactivate conflicting rules of @typescript-eslint/eslint-plugin
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: 'module',
+    allowImportExportEverywhere: true,
+    warnOnUnsupportedTypeScriptVersion: true,
   },
   plugins: [
-    '@typescript-eslint'
+    '@typescript-eslint',
+    'simple-import-sort', // eslint-plugin-simple-import-sort
   ],
   rules: {
-  }
-}
+    semi: [2, 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'arrow-parens': ['error', 'always'],
+    'no-new-object': 'error',
+    'no-array-constructor': 'error',
+    'no-console': 'warn',
+    'no-unused-vars': 'warn',
+    'no-useless-escape': 'warn',
+    // third-party
+    'simple-import-sort/imports': 'error',
+    // typescript
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    '@typescript-eslint/no-var-requires': 'warn',
+  },
+};
