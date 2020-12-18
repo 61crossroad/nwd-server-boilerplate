75634fa400bc265122c291aaa386e5707e903dc8
smallbee3 <smallbee3@gmail.com>
2020-12-18 12:54:18 +0900

    Refactor src/server.ts with process.env.NODE_ENV value and 'dotenv' package

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,27 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import morgan from 'morgan';
+import path from 'path';
 
 import { authenticateJWT } from './apis/auth';
 import routes from './routes';
 
 const app = express();
-const port = 8000;
+
+const env = process.env.NODE_ENV || 'production';
+const envPath =
+  env === 'production'
+    ? '.env'
+    : env === 'development'
+    ? '.env.dev'
+    : '.env.local';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config({
+  path: path.resolve(__dirname, '../' + envPath),
+});
+
+const port = env === 'local' ? 8000 : 80;
 
 app.use(morgan('dev')); // Log every request to the console
 
